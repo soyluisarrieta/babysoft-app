570654f0407f7cbcd44cc20d5d7d6c66f6151e9e
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-03-27 18:43:04 -0500

    Mode screen folder to src, create form field component and add some styles

--- screens/LoginScreen.js
+++
@@ -1,9 +0,0 @@
-import { SafeAreaView, Text } from 'react-native'
-
-export default function LoginScreen () {
-  return (
-    <SafeAreaView>
-      <Text>Login</Text>
-    </SafeAreaView>
-  )
-}
--- screens/ProductsScreen.js
+++
@@ -1,9 +0,0 @@
-import { SafeAreaView, Text } from 'react-native'
-
-export default function ProductsScreen () {
-  return (
-    <SafeAreaView>
-      <Text>Products</Text>
-    </SafeAreaView>
-  )
-}
