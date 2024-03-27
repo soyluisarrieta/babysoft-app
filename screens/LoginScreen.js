c9ece8b28e42469112c1d6258e6076911826646f
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-03-27 17:43:59 -0500

    Add screens

---
+++ screens/LoginScreen.js
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text } from "react-native";
+
+export default function LoginScreen () {
+  return (
+    <SafeAreaView>
+      <Text>Login</Text>
+    </SafeAreaView>
+  )
+}
---
+++ screens/ProductsScreen.js
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text } from "react-native";
+
+export default function ProductsScreen () {
+  return (
+    <SafeAreaView>
+      <Text>Products</Text>
+    </SafeAreaView>
+  )
+}
