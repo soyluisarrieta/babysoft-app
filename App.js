aa1c71bb6e3e93e2f24ff054d04b7b0f15aaa5a4
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-03-27 18:04:42 -0500

    Format projects usign standard rules

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './screens/LoginScreen';
-import ProductsScreen from './screens/ProductsScreen';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import LoginScreen from './screens/LoginScreen'
+import ProductsScreen from './screens/ProductsScreen'
 
 const Stack = createNativeStackNavigator()
 
-export default function App() {
+export default function App () {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name='Login' component={LoginScreen} />
         <Stack.Screen name='Products' component={ProductsScreen} />
       </Stack.Navigator>
-    </NavigationContainer>    
-  );
+    </NavigationContainer>
+  )
 }
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,4 +1,4 @@
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, Text } from 'react-native'
 
 export default function LoginScreen () {
   return (
