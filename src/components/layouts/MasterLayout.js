1d9bcd26084d08d8e76e11de2587cdfdadae281a
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-03-28 15:29:38 -0500

    Add master layout component

---
+++ src/components/layouts/MasterLayout.js
@@ -0,0 +1,19 @@
+import { SafeAreaView, ScrollView, StyleSheet } from 'react-native'
+import { COLORS } from '../../theme'
+
+export default function MasterLayout ({ style, onScroll, children, ...rest }) {
+  return (
+    <SafeAreaView style={[styles.container, style]} {...rest}>
+      {onScroll
+        ? <ScrollView> {children} </ScrollView>
+        : children}
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.white
+  }
+})
