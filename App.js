f0c5b9eab3e1ec7fe47df654bbeab6ecb7810633
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-03-28 08:46:03 -0500

    Create auth context and protected routes

--- App.js
+++ App.js
@@ -1,17 +1,46 @@
+import { useEffect, useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import LoginScreen from './src/screens/LoginScreen'
 import ProductsScreen from './src/screens/ProductsScreen'
+import { profileService } from './src/services/AuthService'
+import AuthContext from './src/contexts/AuthContext'
 
 const Stack = createNativeStackNavigator()
 
 export default function App () {
+  const [profile, setProfile] = useState()
+
+  useEffect(() => {
+    async function runEffect () {
+      try {
+        const profile = await profileService()
+        setProfile(profile)
+      } catch (e) {
+        console.warn('Failed to load user', e)
+      }
+    }
+
+    runEffect()
+  }, [])
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='Login' component={LoginScreen} />
-        <Stack.Screen name='Products' component={ProductsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthContext.Provider value={{ profile, setProfile }}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          {profile
+            ? (
+              <>
+                <Stack.Screen name='Products' component={ProductsScreen} />
+              </>
+              )
+            : (
+              <>
+                <Stack.Screen name='Login' component={LoginScreen} />
+              </>
+              )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthContext.Provider>
   )
 }
