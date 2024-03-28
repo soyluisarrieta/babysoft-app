559c739a0dadddda2d726e211d09d9df1182bc6d
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-03-28 11:41:12 -0500

    Add logo babysoft-logo-circle in SplashScreen

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,9 +1,19 @@
-import { SafeAreaView, Text } from 'react-native'
+import { Image, SafeAreaView, Text, View } from 'react-native'
 
 export default function SplashScreen () {
   return (
-    <SafeAreaView>
-      <Text>Cargando...</Text>
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View>
+        <Image
+          source={require('../../assets/babysoft-logo-circle.jpg')}
+          style={{
+            width: '100%',
+            objectFit: 'contain'
+          }}
+        />
+
+        <Text>Cargando...</Text>
+      </View>
     </SafeAreaView>
   )
 }
