4fd6d0ae9a40e5a051ebcf8c6bc3564ed48f508f
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-03-28 15:28:41 -0500

    Add colors and fonts in theme.js

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+export const COLORS = {
+  primary: { rgb: '53, 145, 216', hex: '#3591d8' },
+  secundary: { rgb: '254, 153, 181', hex: '#fe99b5' },
+  black: { rgb: '50, 59, 67', hex: '#323b43' },
+  white: { rgb: '232, 241, 248', hex: '#e8f1f8' }
+}
+
+export const FONTS = {
+  primary: {
+    light: 'Quicksand-light',
+    regular: 'Quicksand-regular',
+    medium: 'Quicksand-medium',
+    semibold: 'Quicksand-semibold',
+    bold: 'Quicksand-bold'
+  }
+}
