2803aadc3556e9de39f99f8246ee72520a542a60
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-03-28 08:47:45 -0500

    Show user name authenticated to product screen

--- src/screens/ProductsScreen.js
+++ src/screens/ProductsScreen.js
@@ -1,9 +1,13 @@
+import { useContext } from 'react'
 import { SafeAreaView, Text } from 'react-native'
+import AuthContext from '../contexts/AuthContext'
 
 export default function ProductsScreen () {
+  const { profile } = useContext(AuthContext)
+
   return (
     <SafeAreaView>
-      <Text>Products</Text>
+      <Text>Bienvenido a Productos, {profile.name}</Text>
     </SafeAreaView>
   )
 }
