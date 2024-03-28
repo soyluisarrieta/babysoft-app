ce7629a34b834f22bf6d002a8182f95e580a8968
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-03-28 09:34:58 -0500

    Add register route in API with his function

--- API_BABYSOFT/routes/api.php
+++ API_BABYSOFT/routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\Rules\Password;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
 
 Route::group(["middleware" => ["auth:sanctum"]], function(){
   Route::get('/user', function (Request $request) {
@@ -32,6 +35,28 @@ Route::post("/login", function (Request $request){
       'email' => ['The provided credentials are incorrect']
     ]);
   }
+
+  return response()->json([
+    'token' => $user->createToken($request->device_name)->plainTextToken
+  ]);
+});
+
+Route::post("/register", function (Request $request){
+  $request->validate([
+    'name' => ['required', 'string', 'max:255'],
+    'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+    'password' => ['required', 'confirmed', Password::defaults()],
+    'device_name' => ['required'],
+  ]);
+
+  $user = User::create([
+      'name' => $request->name,
+      'email' => $request->email,
+      'password' => Hash::make($request->password),
+  ]);
+
+  event(new Registered($user));
+
 
   return response()->json([
     'token' => $user->createToken($request->device_name)->plainTextToken
