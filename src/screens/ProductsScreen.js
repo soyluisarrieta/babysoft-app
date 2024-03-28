edf841ac972ec25825cac2d0987890c20c123ed4
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-03-28 09:07:46 -0500

    Add logout button and his function in products screen

--- src/screens/ProductsScreen.js
+++ src/screens/ProductsScreen.js
@@ -1,13 +1,20 @@
 import { useContext } from 'react'
-import { SafeAreaView, Text } from 'react-native'
+import { Button, SafeAreaView, Text } from 'react-native'
 import AuthContext from '../contexts/AuthContext'
+import { logoutService } from '../services/AuthService'
 
 export default function ProductsScreen () {
-  const { profile } = useContext(AuthContext)
+  const { profile, setProfile } = useContext(AuthContext)
+
+  const handleLogout = async () => {
+    await logoutService()
+    setProfile(null)
+  }
 
   return (
     <SafeAreaView>
       <Text>Bienvenido a Productos, {profile.name}</Text>
+      <Button title='Cerrar sesión' onPress={handleLogout} />
     </SafeAreaView>
   )
 }
