aaca0cc29fd0436c4c5525e440d73294aa2a7d61
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-03-28 16:21:43 -0500

    Update primary color in theme.js

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,5 @@
 export const COLORS = {
-  primary: { rgb: '53, 145, 216', hex: '#3591d8' },
+  primary: { rgb: '116, 181, 234', hex: '#74b5ea' },
   secundary: { rgb: '254, 153, 181', hex: '#fe99b5' },
   black: { rgb: '50, 59, 67', hex: '#323b43' },
   white: { rgb: '232, 241, 248', hex: '#e8f1f8' }
