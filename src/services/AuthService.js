fc3bed0dabd17c79b61e5ee9a3dfbb5c2d94c560
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-03-28 09:17:17 -0500

    Add axios interceptor to add automatically the bearer token in authorization header

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,5 +1,5 @@
 import axios from '../utils/axios'
-import { getToken, setToken } from './TokenService'
+import { setToken } from './TokenService'
 
 export async function loginService (credentials) {
   const { data } = await axios.post('/login', credentials)
@@ -7,17 +7,11 @@ export async function loginService (credentials) {
 }
 
 export async function profileService () {
-  const token = await getToken()
-  const { data: profile } = await axios.get('/user', {
-    headers: { Authorization: `Bearer ${token}` }
-  })
+  const { data: profile } = await axios.get('/user')
   return profile
 }
 
 export async function logoutService () {
-  const token = await getToken()
-  await axios.post('/logout', {}, {
-    headers: { Authorization: `Bearer ${token}` }
-  })
+  await axios.post('/logout', {})
   await setToken(null)
 }
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,10 +1,21 @@
 import axiosLib from 'axios'
+import { getToken } from '../services/TokenService'
 
 const axios = axiosLib.create({
   baseURL: 'http://192.168.1.105:8000/api',
   headers: {
     Accept: 'application/json'
   }
+})
+
+axios.interceptors.request.use(async (request) => {
+  const token = await getToken()
+
+  if (token !== null) {
+    request.headers.Authorization = `Bearer ${token}`
+  }
+
+  return request
 })
 
 export default axios
