49dccaa783c7159b47bcf7d75c227d2b73dcfb94
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-03-28 07:55:47 -0500

    Use api token sanctum in user model and add route with login function

--- API_BABYSOFT/routes/api.php
+++ API_BABYSOFT/routes/api.php
@@ -1,8 +1,30 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post("/login", function (Request $request){
+  $request->validate([
+    'email' => ['required', 'email'],
+    'password' => ['required'],
+    'device_name' => ['required'],
+  ]);
+
+  $user = User::where('email', $request->email)->first();
+
+  if(!$user || !Hash::check($request->password, $user->password)) {
+    throw ValidationException::withMessages([
+      'email' => ['The provided credentials are incorrect']
+    ]);
+  }
+
+  return response()->json([
+    'token' => $user->createToken($request->device_name)->plainTextToken
+  ]);
 });
