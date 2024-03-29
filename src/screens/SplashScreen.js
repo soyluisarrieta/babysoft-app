8d0e854f467ff96b7ee48b1d12765fda4e8ef47b
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-03-28 21:09:08 -0500

    Add babysoft logo white and add some style in splash screen

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,20 +1,20 @@
-import { Image, Text, View } from 'react-native'
+import { Image } from 'react-native'
 import MasterLayout from '../components/layouts/MasterLayout'
+import { COLORS } from '../theme'
 
 export default function SplashScreen () {
   return (
-    <MasterLayout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <View>
-        <Image
-          source={require('../../assets/babysoft-logo-circle.jpg')}
-          style={{
-            width: '100%',
-            objectFit: 'contain'
-          }}
-        />
-
-        <Text>Cargando...</Text>
-      </View>
+    <MasterLayout style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: COLORS.primary.hex }}>
+      <Image
+        source={require('../../assets/babysoft-logo-white.png')}
+        style={{
+          width: 300,
+          alignSelf: 'center',
+          resizeMode: 'contain',
+          marginBottom: 100,
+          flex: 1
+        }}
+      />
     </MasterLayout>
   )
 }
