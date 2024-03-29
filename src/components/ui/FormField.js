7b2522a11204d006a518b668d31484cfcebca498
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-03-28 19:56:28 -0500

    Add some styles to form field component

--- src/components/ui/FormField.js
+++ src/components/ui/FormField.js
@@ -1,4 +1,5 @@
 import { StyleSheet, Text, TextInput, View } from 'react-native'
+import { FONTS } from '../../theme'
 
 export default function FormField ({ label, errors = [], ...rest }) {
   return (
@@ -27,16 +28,17 @@ export default function FormField ({ label, errors = [], ...rest }) {
 const styles = StyleSheet.create({
   label: {
     color: '#334155',
-    fontWeight: '500'
+    fontFamily: FONTS.primary.semibold,
+    fontSize: 17
   },
   input: {
     backgroundColor: '#f1f5f9',
-    height: 40,
     marginTop: 4,
     borderWidth: 1,
-    borderRadius: 4,
+    borderRadius: 10,
     borderColor: '#cbd5e1',
-    padding: 10
+    paddingVertical: 10,
+    paddingHorizontal: 20
   },
   error: {
     color: 'red',
