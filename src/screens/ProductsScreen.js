bc51625dfad41e32953f4525ffe6742ba076b7ef
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-03-29 09:56:39 -0500

    Show products in products screen without styles

--- src/screens/ProductsScreen.js
+++ src/screens/ProductsScreen.js
@@ -1,8 +1,9 @@
 import { useContext } from 'react'
-import { Button, Text } from 'react-native'
+import { Button, FlatList, StatusBar, StyleSheet, Text, View } from 'react-native'
 import AuthContext from '../contexts/AuthContext'
 import { logoutService } from '../services/AuthService'
 import MasterLayout from '../components/layouts/MasterLayout'
+import { MOCK_PRODUCTS } from '../mocks/products'
 
 export default function ProductsScreen () {
   const { profile, setProfile } = useContext(AuthContext)
@@ -13,9 +14,34 @@ export default function ProductsScreen () {
   }
 
   return (
-    <MasterLayout>
+    <MasterLayout withoutScroll>
       <Text>Bienvenido a Productos, {profile.name}</Text>
+      <FlatList
+        data={MOCK_PRODUCTS}
+        keyExtractor={item => item.id}
+        renderItem={({ item: product }) => (
+          <View style={styles.item}>
+            <Text style={styles.nombre}>{product.nombre}</Text>
+          </View>
+        )}
+      />
       <Button title='Cerrar sesión' onPress={handleLogout} />
     </MasterLayout>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16
+  },
+  title: {
+    fontSize: 32
+  }
+})
