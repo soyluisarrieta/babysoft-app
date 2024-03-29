bbe0746568197dce07ce07a0769d86c7780c8ac7
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-03-28 19:55:40 -0500

    Update white color in theme.js

--- src/theme.js
+++ src/theme.js
@@ -2,7 +2,7 @@ export const COLORS = {
   primary: { rgb: '116, 181, 234', hex: '#74b5ea' },
   secundary: { rgb: '254, 153, 181', hex: '#fe99b5' },
   black: { rgb: '50, 59, 67', hex: '#323b43' },
-  white: { rgb: '232, 241, 248', hex: '#e8f1f8' }
+  white: { rgb: '232, 241, 248', hex: '#eaf4ff' }
 }
 
 export const FONTS = {
