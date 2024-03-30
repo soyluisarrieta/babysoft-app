106316d75e6be7a7854c7861fe8f455046e57145
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-03-30 11:57:34 -0500

    Migrate login system to pos platform api

--- PLATAFORMA_POS/routes/api.php
+++ PLATAFORMA_POS/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(["middleware" => ["auth:sanctum"]], function(){
+  Route::get('/user', function (Request $request) {
+      return $request->user();
+  });
+
+  Route::post('/logout', [AuthenticationController::class, 'logout']);
 });
+
+Route::post("/login", [AuthenticationController::class, 'login']);
+Route::post("/register", [AuthenticationController::class, 'register']);
