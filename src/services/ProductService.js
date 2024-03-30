65ae176e85ee0bd18a0fa353a8dd9c79d125177a
Luis Arrieta Avilez <luisarrieta796@gmail.com>
2024-03-30 18:00:37 -0500

    Add create product service

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -4,3 +4,7 @@ export async function productsService () {
   const { data } = await axios('/productos')
   return data.productos
 }
+
+export async function createProductService (product) {
+  return await axios.post('/productos', product)
+}
