a43d8c52fa9248aff9ce4fd03cac3b96ca71fcf4
Luis Arrieta <luisarrieta796@gmail.com>
2024-04-10 05:23:13 -0500

    Fix spaces in only letters helper

--- babysoftApp/src/utils/helpers.js
+++ babysoftApp/src/utils/helpers.js
@@ -1,5 +1,5 @@
 export const rgb = (rgbCode) => `rgb(${rgbCode})`
 export const rgba = (rgbCode, alpha) => `rgba(${rgbCode}, ${alpha})`
 
-export const onlyNumbers = (text) => text === '' || /^[0-9]+$/.test(text)
-export const onlyLetters = (text) => text === '' || /^[A-Za-z]+$/.test(text)
+export const onlyNumbers = (text) => text.trim() === '' || /^[0-9]+$/.test(text)
+export const onlyLetters = (text) => text.trim() === '' || /^[A-Za-z\s]+$/.test(text)
